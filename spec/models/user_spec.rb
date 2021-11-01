1ffc8182688444a3342c6cefea970eb94b721027
Frantz Crozilhac <fran.stacksta@gmail.com>
2021-11-02 00:43:53 +0100

    Add gem factory_bot_rails

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
+  let!(:app_user) { FactoryBot.create(:user, name:"Jane", spotifyid:'78jdhgeb') }
   it "has many playlists" do
     user = User.reflect_on_association(:playlists)
     expect(user.macro).to eq(:has_many)
   end
   it "should have a unique name-spotifyid combinaison" do
-    User.create!(name:"Jane", spotifyid:'78jdhgeb')
     user_test = User.new(name:"Jane", spotifyid:'78jdhgeb')
 
     expect(user_test).to_not be_valid
