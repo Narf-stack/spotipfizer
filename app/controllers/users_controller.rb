8539ef1d10669dc1241a73f8abd71578a2a8dc8a
Frantz Crozilhac <fran.stacksta@gmail.com>
2021-11-02 19:50:22 +0100

    Add views for User#Spotify

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,15 @@ class UsersController < ApplicationController
 
     def spotify
         spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
-        # Now you can access user's private data, create playlists and much more
         # binding.pry
-
+        
+        @user = User.find_or_create_by(name: spotify_user.display_name, spotifyid:spotify_user.id)
         # @user = User.new(name: spotify_user.display_name, spotifyid:spotify_user.id)
- 
+        if @user
+            render :spotify, notice: "Welcome to a new journey'"
+        else
+            errors = @user.errors.full_messages.to_sentence
+            redirect_to root_path, notice: "Can't logged in - errors :'#{errors}'"
+        end
     end
 end
