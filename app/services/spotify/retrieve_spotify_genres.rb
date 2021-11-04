5c4331a14dc70702c4fc5b9c5dc7ee4492e2e9bd
Frantz Crozilhac <fran.stacksta@gmail.com>
2021-11-04 19:33:28 +0100

    Add Service Retrieving spotify musical genre

---
+++ app/services/spotify/retrieve_spotify_genres.rb
@@ -0,0 +1,18 @@
+class RetrieveSpotifyGenres < ApplicationService
+    MAX_NB_GENRES=5 
+    def self.call(client_spotify_token)
+        headers = {   
+            "Accept"=>"application/json",
+            "Authorization"=>"Bearer #{client_spotify_token}",
+            "Content-Type"=>"application/json"
+        }
+
+        response = HTTParty.get(
+            "https://api.spotify.com/v1/recommendations/available-genre-seeds",
+            :headers => headers
+          )
+
+        JSON.parse(response.body)['genres'].limit(MAX_NB_GENRES)
+    end
+  end
+  
