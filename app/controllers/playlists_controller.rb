adb200e89624d8ec1ddd9e3a99ecd21ac31c3b92
Frantz Crozilhac <fran.stacksta@gmail.com>
2021-11-04 23:27:03 +0100

    Add Service create an empty spotify playlist

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,2 +1,9 @@
 class PlaylistsController < ApplicationController
+    def new
+        @playlist = Playlist.new()
+    end
+
+    def create
+        CreateSpotifyPlaylist.call(curent_user.spotify_access_token, curent_user.spotifyid)
+    end
 end
---
+++ spec/services/spotify/create_spotify_playlist_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe CreateSpotifyPlaylist,type: :service do
+    describe '#call' do
+        let(:client_spotify_token){RSpotify.client_token}
+        let(:spotify_id){RSpotify.client_token}
+
+        it 'return a json of the playlist parameters' do  
+            expect(CreateSpotifyPlaylist).to receive(:call).with(client_spotify_token,spotify_id) 
+
+            url_hash = JSON.parse(response.body).external_urls
+            expect(url_hash.key?("spotify")).to be true
+
+            open_playlist_url = HTTParty.get(url_hash.spotify)
+            expect(open_playlist_url.code).to be 200
+        end
+    end
+end
